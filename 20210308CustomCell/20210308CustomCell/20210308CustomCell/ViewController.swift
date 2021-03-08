0236c0ed7cd1aee64d0024fbdb6d5b6417f1b811
akio0911 <akio0911@gmail.com>
2021-03-08 18:29:29 +0900

    Add CustomCell

---
+++ 20210308CustomCell/20210308CustomCell/20210308CustomCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20210308CustomCell
+//
+//  Created by akio0911 on 2021/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
