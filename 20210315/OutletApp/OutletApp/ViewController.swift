866936971dbc1c7eb99870d30aa121c306088f6b
akio0911 <akio0911@gmail.com>
2021-03-15 17:57:10 +0900

    Add outlet

--- 20210315/OutletApp/OutletApp/ViewController.swift
+++ 20210315/OutletApp/OutletApp/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
