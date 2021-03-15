837bfa7dfb0d3841004da2820630a2e1e57a5abe
akio0911 <akio0911@gmail.com>
2021-03-15 19:31:09 +0900

    ボタン削除

--- 20210315/OutletApp/OutletApp/ViewController.swift
+++ 20210315/OutletApp/OutletApp/ViewController.swift
@@ -10,14 +10,4 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var label: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func didTapButton(_ sender: Any) {
-    }
-    
 }
-
