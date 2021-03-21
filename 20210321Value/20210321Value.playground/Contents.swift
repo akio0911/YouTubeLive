1c62bf48d1dfce71c8b9c45cae2a94ef9d7a615f
akio0911 <akio0911@gmail.com>
2021-03-21 15:46:35 +0900

    Add 20210321Value

---
+++ 20210321Value/20210321Value.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+
+// Int を代入するとコピーされる
+// Int は struct
+
+var a: Int = 10
+var copyOfA: Int = a
+a // 10
+copyOfA // 10
+a = 20
+copyOfA // 10
+
+
+// メソッドに渡した時もコピーされる
+
+var b: Int = 10
+func plusOne(_ copy: Int) -> Int {
+    // Left side of mutating operator isn't mutable: 'num' is a 'let' constant
+    //num += 1
+
+    return copy + 1
+}
+plusOne(b)
+b
+
+
+// 独自に作った struct もコピーされる
+
+struct Person {
+    var age: Int
+}
+
+var sato = Person(age: 20)
+var copyOfSato = sato
+
+sato.age // 20
+sato.age += 1
+sato.age // 21
+
+copyOfSato.age // 20
