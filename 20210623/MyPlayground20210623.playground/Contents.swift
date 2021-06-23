dff6c0a4a2e3707d8bd1ce26c5b830d8659a5809
akio0911 <akio0911@gmail.com>
2021-06-23 18:49:19 +0900

    2021/06/23のPlaygroundを追加

---
+++ 20210623/MyPlayground20210623.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+
+
+func add1(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+add1(a: 10, b: 20)
+
+
+
+
+func add2(a: Int, b: Int) -> Int {
+    a + b
+}
+
+add2(a: 10, b: 20)
+
+
+
+
+func add3(a: Int, b: Int) -> Int {
+    let result = a + b
+    return result
+}
+
+add3(a: 10, b: 20)
