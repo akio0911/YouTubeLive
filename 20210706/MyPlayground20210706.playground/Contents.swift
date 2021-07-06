9ceb69798e1c2a98d8018d132e2e49a09bd46ce7
akio0911 <akio0911@gmail.com>
2021-07-06 18:42:36 +0900

    Add 20210706

---
+++ 20210706/MyPlayground20210706.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+
+
+var isWinner = true
+
+
+
+
+
+
+
+var score = 0
+if isWinner {
+    score = 100
+} else {
+    score = 0
+}
+
+score = -999
+
+
+
+
+
+
+//let score: Int
+//if isWinner {
+//    score = 100
+//} else {
+//    score = 0
+//}
+//
+//score = -999
+
+
+
+
+
